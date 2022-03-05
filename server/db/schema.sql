de5b912455755210ad014057a25e8ad61f5dad56
Dylan Honeyman <dylan@dylans-iMac.local>
2022-03-05 14:30:34 -0600

    more stuff

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -8,7 +8,8 @@ CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    email VARCHAR(30) NOT NULL
+    email VARCHAR(30) NOT NULL,
+    password
 );
 
 CREATE TABLE listings (
